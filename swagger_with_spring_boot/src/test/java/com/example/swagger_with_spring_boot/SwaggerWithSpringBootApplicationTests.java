a32de47923328ac049d649c3ba9e9ca3461e255a
fesabelilla <fesabelilla@gmail.com>
2022-10-31 10:25:27 +0600

    Swagger2 implementation done

---
+++ swagger_with_spring_boot/src/test/java/com/example/swagger_with_spring_boot/SwaggerWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swagger_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
