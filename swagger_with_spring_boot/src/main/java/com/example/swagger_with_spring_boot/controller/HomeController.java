9a12197816cd86882b7eb4dc287a015bf08bad94
fesabelilla <fesabelilla@gmail.com>
2022-10-31 10:30:28 +0600

    Improvement

--- swagger_with_spring_boot/src/main/java/com/example/swagger_with_spring_boot/controller/HomeController.java
+++ swagger_with_spring_boot/src/main/java/com/example/swagger_with_spring_boot/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.example.swagger_with_spring_boot.controller;
 
+import com.example.swagger_with_spring_boot.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public ResponseEntity<String> getWelcomeMsg() {
-        return new ResponseEntity<>( "Welcome Home", HttpStatus.OK);
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
--- swagger_with_spring_boot/src/main/java/com/example/swagger_with_spring_boot/model/User.java
+++ swagger_with_spring_boot/src/main/java/com/example/swagger_with_spring_boot/model/User.java
@@ -1,4 +1,52 @@
 package com.example.swagger_with_spring_boot.model;
 
+
 public class User {
+
+    private Long id;
+    private Long name;
+    private String email;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(Long id, Long name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getName() {
+        return name;
+    }
+
+    public void setName(Long name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
